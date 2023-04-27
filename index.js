0351d2aa544172de7e782015f97e782ee24c298d
Igor Abdulagadzhiev <s89183906500@gmail.com>
2023-04-27 12:54:26 +0300

    refactoring imports

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ import multer from 'multer';
 import mongoose from 'mongoose';
 
 import { registerValidation, loginValidation } from './validations/auth.js';
+import { checkAuth, handleValidationErrors } from './utils/index.js';
 import { postCreateValidation } from './validations/post.js';
 
-import checkAuth from './utils/checkAuth.js';
-
-import * as UserController from './controllers/UserController.js';
-import * as PostController from './controllers/PostController.js';
+import { UserController, PostController } from './controllers/index.js';
 
 const PORT = 4444;
 const URL =
@@ -24,7 +22,7 @@ const app = express();
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
-    cb(null, 'uploads')
+    cb(null, 'uploads');
   },
   filename: (_, file, cb) => {
     cb(null, file.originalname);
@@ -50,14 +48,36 @@ app.post('/upload', upload.single('image'), (req, res) => {
 // posts
 app.get('/posts', PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
-app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+app.post(
+  '/posts',
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.create
+);
 app.delete('/posts/:id', checkAuth, PostController.remove);
-app.patch('/posts/:id', checkAuth, PostController.update);
+app.patch(
+  '/posts/:id',
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.update
+);
 
 // auth
 app.get('/auth/me', checkAuth, UserController.getMe);
-app.post('/auth/login', loginValidation, UserController.login);
-app.post('/auth/register', registerValidation, UserController.register);
+app.post(
+  '/auth/login',
+  loginValidation,
+  handleValidationErrors,
+  UserController.login
+);
+app.post(
+  '/auth/register',
+  registerValidation,
+  handleValidationErrors,
+  UserController.register
+);
 
 app.listen(PORT, (err) => {
   if (err) {
